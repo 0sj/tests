d8c2abd5ee4d18fea5d1d1e64be2746ea5b1d322
Jaydn <jaydnisswag@gmail.com>
2024-05-22 18:12:58 +1200

    augh

---
+++ currency converter.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("what is the current value of the NZD in comparison to the target currency");
+        decimal NZD = Convert.ToDecimal(Console.ReadLine());
+
+    Console.WriteLine("how much money does the item cost :3 \n:");
+
+        decimal Amount = Convert.ToDecimal( Console.ReadLine()) * NZD;
+
+        Console.WriteLine("you have " + Amount + "NZD");
+    }
+}
+//meowemdfnjvsdfbnvjdfsgbfgebknb nk
--- obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e03d38b53af3821b4e4e78fd185c20f9f96a4e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
