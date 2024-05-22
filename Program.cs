8e03d38b53af3821b4e4e78fd185c20f9f96a4e1
Jaydn <jaydnisswag@gmail.com>
2024-05-22 18:06:20 +1200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("what is the current value of the NZD in comparison to USD");
+        decimal NZD = Convert.ToDecimal(Console.ReadLine());
+
+Console.WriteLine("how much money have you gots :3 \t\t:");
+
+        decimal Amount = Convert.ToDecimal( Console.ReadLine()) * NZD;
+
+        Console.WriteLine("you have " + Amount);
+    }
+}
